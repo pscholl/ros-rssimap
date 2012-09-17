944b23f20c601c31181f397967cb1a2583959b87
Philipp Scholl <pscholl@bawue.de>
2012-09-17 23:40:22 +0000

    correctly retrieve current position from tf
     - publish tf from /map -> /base_link as position of rssi measurement (via
       rssi-pose.py)
    
     - fix RssiPoseStamped accordingly
    
     - more topics in visualizer.launch published

---
+++ src/rssi-pose.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('rssimap')
+import rospy, tf
+from geometry_msgs.msg import PoseStamped
+from rssimap.msg import RssiStamped, RssiPoseStamped
+from numpy import arange
+
+pub,lis = None,None
+
+def rssi(data):
+    try:
+        (trans,rot) = lis.lookupTransform('/map', '/base_link', rospy.Time(0))
+        msg = RssiPoseStamped()
+        msg.rssi = data
+        msg.pose.position.x,msg.pose.position.y,msg.pose.position.z=trans
+        msg.pose.orientation.x,msg.pose.orientation.y,\
+                msg.pose.orientation.z,msg.pose.orientation.w=rot
+        pub.publish(msg)
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        pass
+
+    #msg.header.stamp = rospy.Time.now()
+
+def rssi_pose_merge():
+    global pub,lis
+    rospy.init_node('rssi_pose')
+    pub=rospy.Publisher('rssi_pose', RssiPoseStamped)
+    lis=tf.TransformListener()
+    rospy.Subscriber('rssi', RssiStamped, rssi)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try: rssi_pose_merge()
+    except rospy.ROSInterruptException:
+        pass
