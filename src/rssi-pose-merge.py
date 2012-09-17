b077706a26b28c7e55d6a8c6b26b757ca0282aa9
Philipp Scholl <pscholl@bawue.de>
2012-09-17 13:45:20 +0000

    add pose merge and update launch file
     rssi and pose will now be published on /rssi_pose_out, combining the last
    reported pose with the current rssi measurement

---
+++ src/rssi-pose-merge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('rssimap')
+import rospy
+from geometry_msgs.msg import PoseStamped
+from rssimap.msg import RssiStamped, RssiPoseStamped
+from numpy import arange
+
+pub = None
+curpose = None
+
+def rssi(data):
+    msg = RssiPoseStamped(pose=curpose, rssi=data)
+    msg.header.stamp = rospy.Time.now()
+    pub.publish(msg)
+
+def pose(data):
+    global curpose
+    curpose = data
+
+def rssi_pose_merge():
+    global pub
+    rospy.init_node('rssi_pose')
+    pub=rospy.Publisher('rssi_pose_out', RssiPoseStamped)
+    rospy.Subscriber("rssi_out", RssiStamped, rssi)
+    rospy.Subscriber("slam_out_pose", PoseStamped, pose)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try: rssi_pose_merge()
+    except rospy.ROSInterruptException:
+        pass
